5497868641074fb58142c426a4fb637a156c8ef7
mike <mikerambil@gmail.com>
2023-06-04 00:17:39 -0500

    Taking B 1:00:44

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,40 +1,28 @@
 import '@styles/globals.css';
 
-// // import Nav from '@components/Nav';
-// // import Provider from '@components/Provider';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
   title: 'Prompt-GPT',
   description: 'Discover & Share AI Prompts',
 };
 
-const RootLayout = ({ children }) => {
-  return (
-    <html lang='en'>
-      <body>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
-        <main className='app'>{children}</main>
-      </body>
-    </html>
-  );
-};
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      {/* <Provider> */}
+      <div className='main'>
+        <div className='gradient' />
+      </div>
+
+      <main className='app'>
+        <Nav />
+        {children}
+      </main>
+      {/* </Provider> */}
+    </body>
+  </html>
+);
 
 export default RootLayout;
-// const RootLayout = ({ children }) => (
-//   <html lang='en'>
-//     <body>
-//       {/* <Provider> */}
-//       <div className='main'>
-//         <div className='gradient' />
-//       </div>
-
-//       <main className='app'>
-//         {/* <Nav /> */}
-//         {children}
-//       </main>
-//       {/* </Provider>*/}
-//     </body>
-//   </html>
-// );
